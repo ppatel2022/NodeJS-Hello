4539a202aaff6bb2bec111da13117d8195803873
ppatel <ppatel@marinerfinance.com>
2021-06-05 17:30:07 +0000

    p

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
- 
-    response.end(`Marketing lookup is Down, Response status : ${status} \n Name: ${firstName}  ${LastName} \n SSN: **${Last7Social} \n Offer Code: ${OfferCode} `);
+ var status = 'Defined';
+ var firstName = 'Paresh';
+ var LastName = 'Patel';
+ var Last7Social = '9843543';
+ var OfferCode = '4343233';
 
+    response.end(`Marketing lookup is Down, Response status : ${status} \n Name: ${firstName}  ${LastName} \n SSN: **${Last7Social} \n Offer Code: ${OfferCode} MY name is PARESH.`);
+    
+    
 });
 
-var port = 80;
+var port = 5000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
