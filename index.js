a8035dee9f2325a42f3581c3eb8b336eddc56f9f
ppatel <ppatel@marinerfinance.com>
2021-06-05 16:29:05 +0000

    dockerfile1

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+ 
+    response.end(`Marketing lookup is Down, Response status : ${status} \n Name: ${firstName}  ${LastName} \n SSN: **${Last7Social} \n Offer Code: ${OfferCode} `);
 
 });
 
